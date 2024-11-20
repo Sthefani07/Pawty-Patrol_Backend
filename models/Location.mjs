576df8991a61afffcb7bedce79935c7aae2b01cf
Sthefani Denhof <sdenhof7@gmail.com>
2024-11-19 17:17:02 -0800

    added review to the locationSchema

--- models/Location.mjs
+++ models/Location.mjs
@@ -11,6 +11,12 @@ const locationShema = new mongoose.Schema({
         coordinates: { type: [Number], required: true }, // [longitude, latitude, find local pet events]
     },
     description: {type: String},
+    review: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        text: {type: String, required: true},
+        rating: {type: Number, require: true, min: 1, max: 5},
+        date: { type: Data, default: Data.now}
+    }]
 });
 
 
